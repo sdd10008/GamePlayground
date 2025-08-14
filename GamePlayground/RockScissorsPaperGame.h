5ec75ba473b45faf9e9e682158ed599b1f6a3874
taka1231 <taka9638918@gmail.com>
2025-08-15 02:20:19 +0900

    Add Rock Scissors Paper Game header file

---
+++ GamePlayground/RockScissorsPaperGame.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Game.h"
+#include <string>
+class RockScissorsPaperGame :
+    public Game
+{
+public:
+    std::string getName() const override {
+        return "Rock Scissors Paper Game";
+    }
+
+    void startGame() override;
+};
+
