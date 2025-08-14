1553d157e892dd041cf952d384f053ee2eb58311
taka1231 <taka9638918@gmail.com>
2025-08-15 02:03:43 +0900

    Add define variable

--- GamePlayground/NumberGuessingGame.h
+++ GamePlayground/NumberGuessingGame.h
@@ -10,5 +10,13 @@ public:
     }
 
     void startGame() override;
+
+private:
+    int _min_random_number = 0;
+    int _max_random_number;
+    int _difficulty_level;
+    int _continue_count = 10;
+    int _expect_number;
+    char _continue_input;
 };
 
