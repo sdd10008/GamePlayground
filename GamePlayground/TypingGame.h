285fd602396fb8c17983070b9b0e6caee8676ff8
taka1231 <taka9638918@gmail.com>
2025-09-15 02:04:10 +0900

    Add define typing work list

--- GamePlayground/TypingGame.h
+++ GamePlayground/TypingGame.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Game.h"
 #include <string>
+#include <vector>
 class TypingGame :
     public Game
 {
@@ -12,5 +13,10 @@ public:
     void startGame() override;
 
     // TODO define variable
+private:
+    int _difficulty_level;
+    std::vector<std::string> _current_word_list;
+    const std::vector<std::string> EASY_WORD_LIST = {"cat","dog","sun","run","hat","red","big","map","box","jump","fish","ball","book","tree","cake","bird","milk","blue","good","door"};
+    const std::vector<std::string> NORMAL_WORD_LIST = {"window","orange","flower","school","family","pretty","banana","yellow","travel","planet","garden","dinner","camera","turtle","market","summer","doctor","pocket","button","animal"};
+    const std::vector<std::string> HARD_WORD_LIST = {"schedule","psychology","astronaut","thermometer","electricity","dictionary","university","architecture","opportunity","complicated","refrigerator","environment","mathematics","transportation","championship","questionnaire","communication","advertisement","responsibility","entrepreneurship"};
 };
-
