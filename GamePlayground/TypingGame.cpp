411a1600aa9ac29490be8aadde8b91af99d3cf17
taka1231 <taka9638918@gmail.com>
2025-09-15 02:02:55 +0900

    Add select difficulty level logic

--- GamePlayground/TypingGame.cpp
+++ GamePlayground/TypingGame.cpp
@@ -1,9 +1,30 @@
 #include "TypingGame.h"
 #include <iostream>
+
 void TypingGame::startGame() {
 	std::cout << "Start Typing Game!" << std::endl;
 
-	// TODO select difficulty level
+	// select difficulty level
+	while (true) {
+		std::cout << "Please choose a difficulty level.1:easy 2:normal 3:hard" << std::endl;
+		std::cin >> _difficulty_level;
+		if (_difficulty_level == 1) {
+			std::cout << "Choose easy mode." << std::endl;
+			_current_word_list = EASY_WORD_LIST;
+			break;
+		}
+		if (_difficulty_level == 2) {
+			std::cout << "Choose normal mode" << std::endl;
+			_current_word_list = NORMAL_WORD_LIST;
+			break;
+		}
+		if (_difficulty_level == 3) {
+			std::cout << "Choose hard mode" << std::endl;
+			_current_word_list = HARD_WORD_LIST;
+			break;
+		}
+		std::cout << "Invalid value" << std::endl;
+	}
 
 	// TODO start typing game
 
