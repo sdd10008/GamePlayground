ffdf4fa5388680722c9e81ecfaacb3f5b8a71c79
taka1231 <taka9638918@gmail.com>
2025-09-15 22:46:19 +0900

    Add Black Jack class

---
+++ GamePlayground/BlackJack.cpp
@@ -0,0 +1,7 @@
+#include "BlackJack.h"
+#include <iostream>
+
+void BlackJack::startGame() {
+	std::cout << "Start Black Jack Game!" << std::endl;
+	std::cout << "Thank you for playing." << std::endl;
+}
